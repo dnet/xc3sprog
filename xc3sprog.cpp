ecb4e476ef49084c499e9454d3f185407cd6d7c8
András Veres-Szentkirályi <vsza@vsza.hu>
2010-05-18 19:53:30 +0000

    Added missing includes
    g++ wouldn't have compiled the code without them

--- xc3sprog.cpp
+++ xc3sprog.cpp
@@ -25,6 +25,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 #include "devicedb.h"
 #include "progalgxcf.h"
 #include "progalgxc3s.h"
+#include <string.h>
+#include <stdlib.h>
 
 #define PPDEV "/dev/parport0"
 #define DEVICEDB "devlist.txt"
